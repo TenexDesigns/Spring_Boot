c879e571e2c4d1be04902953cd728bd8ab038744
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-17 20:31:55 +0300

    Create Validations_Deoency_Impl_2.java

---
+++ Validation annotations/Validations_Deoency_Impl_2.java
@@ -0,0 +1,40 @@
+
+Yes, to enable validation annotations to work in a Spring Boot application, you need to include the appropriate dependency and configure the validation process. Here are the steps:
+
+1. **Include Validation Dependency**:
+   Add the `javax.validation` dependency to your project. In a Maven-based project, you can include it like this in your `pom.xml`:
+
+   ```xml
+   <dependency>
+       <groupId>javax.validation</groupId>
+       <artifactId>validation-api</artifactId>
+   </dependency>
+   ```
+
+   For Gradle, add this to your `build.gradle`:
+
+   ```groovy
+   implementation 'javax.validation:validation-api'
+   ```
+
+2. **Enable Validation in Spring Boot**:
+   By default, Spring Boot auto-configures validation support.
+However, you need to ensure that your Spring Boot application has the appropriate validation starter dependencies. 
+The starter module includes validation-related configurations.
+
+   If you're using Spring Boot's starter parent, it will include validation by default.
+If you're manually managing your dependencies, ensure you have the relevant starters like `spring-boot-starter-validation`.
+
+
+<dependency> 
+    <groupId>org.springframework.boot</groupId> 
+    <artifactId>spring-boot-starter-validation</artifactId> 
+</dependency>
+
+
+That's it! With these steps, validation annotations like `@NotNull`, `@Size`, `@Email`, etc., should work in your Spring Boot application.
+
+Remember that the validation errors will be automatically processed by Spring and can be accessed in the `BindingResult` object in your controller methods. You can customize error messages using properties files or annotations in your entity classes.
+
+It's also worth noting that you can create custom validation annotations by combining existing ones or by creating your own custom validation logic using the `javax.validation.Constraint` interface.
+
