40282a9b655412502c9c7a48e27ef4505657d5f7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-25 17:48:39 +0300

    Update 2_Manual_Use_Of_SqL_as_There_is_No_ORM.java

--- Flyway /2_Manual_Use_Of_SqL_as_There_is_No_ORM.java
+++ Flyway /2_Manual_Use_Of_SqL_as_There_is_No_ORM.java
@@ -6,7 +6,9 @@ SO waht you are saying is that the relationships, eager loading nd thr things we
 
 
 
-That's correct. When using Flyway (or other database migration tools like Liquibase) in a Spring Boot application, you are responsible for managing your database schema changes manually, including creating tables, defining relationships, specifying eager loading, and other aspects that are typically handled automatically by ORMs like Django and Rails.
+That's correct. When using Flyway (or other database migration tools like Liquibase) in a Spring Boot application,
+   you are responsible for managing your database schema changes manually, including creating tables, defining relationships, 
+   specifying eager loading, and other aspects that are typically handled automatically by ORMs like Django and Rails.
 
 Here's a breakdown of what this means:
 
@@ -14,15 +16,22 @@ Here's a breakdown of what this means:
    In Spring Boot with Flyway, you create SQL migration scripts manually to define changes to your database schema. This includes creating tables, adding columns, defining indexes, setting constraints, etc. This process is more hands-on compared to how ORMs like Django and Rails can automatically create tables and handle schema changes based on your entity classes.
 
 2. **Relationships and Eager Loading**:
-   When dealing with relationships between entities and eager loading in an ORM like Django or Rails, you would use annotations or configuration to specify how these relationships should be mapped to the database and how data should be fetched. In Spring Boot, when using Flyway, you'll need to manage relationships and loading strategies explicitly in your SQL scripts.
+   When dealing with relationships between entities and eager loading in an ORM like Django or Rails, you would use annotations or configuration to 
+   specify how these relationships should be mapped to the database and how data should be fetched. 
+   In Spring Boot, when using Flyway, you'll need to manage relationships and loading strategies explicitly in your SQL scripts.
 
 3. **Data Access Code**:
-   In Spring Boot, while you're responsible for managing the database schema changes manually, you can still use JPA (Java Persistence API) to interact with your database in a more object-oriented manner. JPA abstracts away many of the low-level SQL operations, making it easier to perform CRUD (Create, Read, Update, Delete) operations on your data.
+   In Spring Boot, while you're responsible for managing the database schema changes manually, you can still use JPA (Java Persistence API)
+ to interact with your database in a more object-oriented manner. 
+   JPA abstracts away many of the low-level SQL operations, making it easier to perform CRUD (Create, Read, Update, Delete) operations on your data.
 
 4. **Customization and Control**:
-   While the process might be more manual, it also provides you with more customization and control over your database schema and interactions. You have the flexibility to design your schema exactly as you need it and write complex SQL queries when necessary.
+   While the process might be more manual, it also provides you with more customization and control over your database schema and interactions. 
+   You have the flexibility to design your schema exactly as you need it and write complex SQL queries when necessary.
 
-It's worth noting that the decision to use Flyway or other migration tools vs. an ORM depends on your project's requirements and your team's preferences. ORMs abstract away many of the database details and can speed up development, but they might not offer the same level of control and customization as manual schema management with migration tools.
+It's worth noting that the decision to use Flyway or other migration tools vs. an ORM depends on your project's requirements and your team's preferences.
+   ORMs abstract away many of the database details and can speed up development, but they might not offer the same level of control and customization as manual
+   schema management with migration tools.
 
 
 
